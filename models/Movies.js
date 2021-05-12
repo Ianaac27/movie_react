9b6f7fa54ae7b6193ca93b2887c65fbdc3ee06e7
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-05-12 15:56:41 -0700

    developed movie model

---
+++ models/Movies.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    Title: String,
+    Year: String,
+    Rated: String,
+    Runtime: String,
+    Director: String,
+    Actors: [String],
+    Plot: String,
+    Metascore: String
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
