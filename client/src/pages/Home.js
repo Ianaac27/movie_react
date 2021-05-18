55d7eac454dad50dabdca1791b8eeceeed59b2af
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-05-18 14:08:33 -0700

    installed react-router-dom, set up initial components for app functionality

---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+function Home () {
+// const [movies, setMovies] = useState([]);
+  // return ( <div></div>
+  // );
+
+}
+
+export default Home;
---
+++ client/src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const API = {
+
+  // Gets all movies
+  getMovies: function() {
+    return axios.get("/api/movies");
+  },
+  // Saves a movie to the database
+  saveMovie: function(movieData) {
+    console.log(movieData)
+    return axios.post("/api/movies", movieData);
+  },
+   // Deletes the movie with the given id
+   deleteMovie: function(id) {
+    return axios.delete("/api/movies/" + id);
+  }
+
+};
+
+export default API;
