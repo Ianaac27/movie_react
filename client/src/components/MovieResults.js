51795c2fd48ba2a95d81bcd79890741a1ad03fab
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-05-25 16:42:15 -0700

    updated home layout for movie search list and favorites list

--- client/src/components/MovieResults.js
+++ client/src/components/MovieResults.js
@@ -5,12 +5,14 @@ function MovieResults({movies, apiKey, setSavedMovies}) {
 
     return (
     <>
-        {movies.map((movie, index)=>
+        {movies.length > 0 ? movies.map((movie, index)=>
         <div className="w-25 h-25">
                 <img  src={movie.Poster} alt={movie.Title}/>
                 <SaveButton movie={movie} apiKey={apiKey} setSavedMovies={setSavedMovies}/>
         </div>
-        )}
+        ):  <div className="col">
+                <h3 className="fs-1 text-white text-center mt-5">Search for any movie!</h3>
+            </div>}
     </>
     )
 }
--- client/src/components/SavedMovies.js
+++ client/src/components/SavedMovies.js
@@ -4,14 +4,19 @@ import DeleteButton from "./DeleteButton";
 function SavedMovies ({savedMovies, setSavedMovies}) {
 
     return (
-        <>
-        {savedMovies.length > 0 ? savedMovies.map((movie, index)=>
-        <div className="w-25 h-25">
-                <img  src={movie.poster} alt={movie.title}/>
-                <DeleteButton id={movie._id} setSavedMovies={setSavedMovies} />
+        <div className="col">
+             <h1 className="fs-1 text-white text-center mt-5">Favorites</h1>
+             <div className="row" id="faveMovies">
+                {savedMovies.length > 0 ? savedMovies.map((movie, index)=>
+                <div className="w-25 h-25">
+                        <img  src={movie.poster} alt={movie.title}/>
+                        <DeleteButton id={movie._id} setSavedMovies={setSavedMovies} />
+                </div>
+                ):  <div className="col">
+                        <h3 className="fs-1 text-white text-center mt-5">You have no saved movies</h3>  
+                    </div>}
+            </div>
         </div>
-        ): <h3 className="fs-1 text-white text-center mt-5">You have no saved movies</h3>}  
-        </>
 )
 }
 
