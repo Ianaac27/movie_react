0f81d7e332ff4e86ced65d9c3eb4cf9eba4c2aa3
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-05-28 16:33:50 -0700

    applied card and styling to module, updated stying of close icon

--- client/src/components/MovieModule.js
+++ client/src/components/MovieModule.js
@@ -1,5 +1,6 @@
 import React from "react";
-// import "../App.css";
+import { Card, Button } from "react-bootstrap";
+import "../App.css";
 
 
 function MovieModule({selectedMovie, handleModuleLink}) {
@@ -7,12 +8,31 @@ function MovieModule({selectedMovie, handleModuleLink}) {
     console.log(selectedMovie)
     
     return (
-        <div className="module">
-            <div className="mod-main">
-                <button className="mod-close" onClick={handleModuleLink}>Close</button>
-                <img className="movie-poster" src={selectedMovie.poster} alt=""/>
-            </div>
+    <Card className="module" >
+        <div id="close-icon" className="mod-close" onClick={handleModuleLink}>
+            <span class="close-bar"></span>
+            <span class="close-bar"></span>
         </div>
+        <Card.Body className="mod-main d-flex justify-content-center align-item-center m-5">
+            <div className="movie-info d-flex flex-column w-50">
+                <h5>{selectedMovie.title}</h5>
+                <p>{selectedMovie.rated}</p>
+                <p>{selectedMovie.year}</p>
+                <p>Runtime: {selectedMovie.runtime}</p>
+                <p>Starring: {selectedMovie.actors}</p>
+                <p>Directed by: {selectedMovie.director}</p>
+                <p>{selectedMovie.plot}</p>
+                <p>Metascore: {selectedMovie.metascore}</p>
+            </div>
+            <div className="movie-sources d-flex flex-column ml-4">
+                <img className="movie-poster" src={selectedMovie.poster} alt=""/>
+                <div className="movie-links d-flex flex-row mt-2">
+                    <Button className="link-btn mr-2">Watch the Trailer</Button>
+                    <Button className="link-btn">Purchase this {selectedMovie.type}</Button>
+                </div>
+            </div>
+        </Card.Body>
+    </Card>
     )
 }
 
