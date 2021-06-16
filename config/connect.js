bb5ca391ceaabd9e715b9c48f1ac701497772a50
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-06-16 00:42:40 -0700

    deploying to heroku

--- config/connect.js
+++ config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// Connect to the Mongo DB
+// Connect to the Mongo DB 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/moviereact",
 {
     useNewUrlParser: true,
